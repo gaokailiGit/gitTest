18695644e76e7d2e609e9afdafa51d1b403eb3c1
gkl <gkl@qq.com>
2022-05-03 14:09:36 +0800

    fenzhi2 first commit

--- src/main/java/com/gkl/GitTest.java
+++ src/main/java/com/gkl/GitTest.java
@@ -3,5 +3,6 @@ package com.gkl;
 public class GitTest {
     public static void main(String args[]){
         System.out.println("hello java");
+        System.out.println("hello fenzhi2");
     }
 }
